4bb287f2f86902e117cc097286462da9625ac045
Guo KK <kkguokk@gmail.com>
2020-03-28 00:33:56 -0400

    Add files via upload

---
+++ Tutorial/Lesson 21 Relay/Relay_Code/Relay_Code.ino
@@ -0,0 +1,41 @@
+#define E1 5 // Enable Pin for motor
+#define I1 3 // Control pin 1 for motor 
+#define I2 4 // Control pin 2 for motor 
+
+int i;
+ 
+void setup() {
+  //---set pin direction
+  pinMode(E1,OUTPUT);
+  pinMode(I1,OUTPUT);
+  pinMode(I2,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+ 
+//---back and forth example
+    Serial.println("One way, then reverse");
+    digitalWrite(E1,HIGH); // enable on
+    for (i=0;i<5;i++) {
+    digitalWrite(I1,HIGH); //one way
+    digitalWrite(I2,LOW);
+    delay(1000);
+    digitalWrite(I1,LOW);  //reverse
+    digitalWrite(I2,HIGH);
+    delay(1000);
+  }
+  digitalWrite(E1,LOW); // disable
+    delay(3000);
+      for (i=0;i<5;i++) {
+    digitalWrite(I1,HIGH); //one way
+    digitalWrite(I2,LOW);
+    delay(1000);
+    digitalWrite(I1,LOW);  //reverse
+    digitalWrite(I2,HIGH);
+    delay(1000);
+  }
+    digitalWrite(E1,LOW); // disable
+    delay(3000);
+}
+   
